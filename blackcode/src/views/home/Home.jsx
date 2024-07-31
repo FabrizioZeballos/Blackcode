14e38e905e1f895006c2f8412a1176ac3de05646
FabrizioZeballos <zeballosfab@gmail.com>
2024-07-31 12:49:50 -0300

    Add social links placeholders and work on small details

--- blackcode/src/views/home/Home.jsx
+++ blackcode/src/views/home/Home.jsx
@@ -1,8 +1,10 @@
-import { FaLinkedin } from "react-icons/fa";
+import {
+  FaLinkedin,
+  FaInstagram,
+  FaFacebook,
+  FaWhatsapp,
+} from "react-icons/fa";
 import { FaXTwitter } from "react-icons/fa6";
-import { FaInstagram } from "react-icons/fa";
-import { FaFacebook } from "react-icons/fa";
-import { FaWhatsapp } from "react-icons/fa";
 import styles from "./Home.module.css";
 import backgroundVideo from "../../assets/temporary-background.mp4";
 
@@ -18,14 +20,46 @@ export const Home = () => {
       ></video>
       <div className={styles["title-container"]}>
         <h1 className={styles.title}>b/ackcode_</h1>
-        <p className={styles.email}>inquiries@blackcode.ch</p>
+        <a href={`mailto:inquiries@blackcode.ch`} className={styles.email}>
+          inquiries@blackcode.ch
+        </a>
       </div>
       <div className={styles.icons}>
-        <FaLinkedin className={styles.icon} />
-        <FaXTwitter className={styles.icon} />
-        <FaInstagram className={styles.icon} />
-        <FaFacebook className={styles.icon} />
-        <FaWhatsapp className={styles.icon} />
+        <a
+          href="https://www.linkedin.com/in/yourprofile"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaLinkedin className={styles.icon} />
+        </a>
+        <a
+          href="https://twitter.com/yourprofile"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaXTwitter className={styles.icon} />
+        </a>
+        <a
+          href="https://www.instagram.com/yourprofile"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaInstagram className={styles.icon} />
+        </a>
+        <a
+          href="https://www.facebook.com/yourprofile"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaFacebook className={styles.icon} />
+        </a>
+        <a
+          href="https://wa.me/yourphonenumber"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaWhatsapp className={styles.icon} />
+        </a>
       </div>
     </div>
   );
