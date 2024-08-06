e8b470684a7974bcf50fb10368cd7afb11586259
FabrizioZeballos <zeballosfab@gmail.com>
2024-08-06 09:58:08 -0300

    Add playsInline video attribute

--- blackcode/src/views/home/Home.jsx
+++ blackcode/src/views/home/Home.jsx
@@ -6,39 +6,14 @@ import {
 } from "react-icons/fa";
 import { FaXTwitter } from "react-icons/fa6";
 import styles from "./Home.module.css";
-import backgroundVideo from "../../assets/temporary-background.mp4";
-import { useEffect, useRef } from "react";
+import backgroundVideo from "/temporary-background.mp4";
 
 export const Home = () => {
-  const videoRef = useRef(null);
-
-  useEffect(() => {
-    if (videoRef.current) {
-      videoRef.current.load();
-
-      const playVideo = () => {
-        videoRef.current.play().catch((error) => {
-          console.error("Error attempting to play video:", error);
-        });
-      };
-
-      videoRef.current.addEventListener("canplaythrough", playVideo, {
-        once: true,
-      });
-
-      return () => {
-        if (videoRef.current) {
-          videoRef.current.removeEventListener("canplaythrough", playVideo);
-        }
-      };
-    }
-  }, []);
-
   return (
     <div className={styles.container}>
       <video
-        ref={videoRef}
         src={backgroundVideo}
+        autoPlay
         loop
         muted
         playsInline
@@ -51,40 +26,21 @@ export const Home = () => {
           inquiries@blackcode.ch
         </a>
       </div>
+      {/* Add href attribute whenever social links are done */}
       <div className={styles.icons}>
-        <a
-          /* href="https://www.linkedin.com/in/yourprofile" */
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a target="_blank" rel="noopener noreferrer">
           <FaLinkedin className={styles.icon} />
         </a>
-        <a
-          /* href="https://twitter.com/yourprofile" */
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a target="_blank" rel="noopener noreferrer">
           <FaXTwitter className={styles.icon} />
         </a>
-        <a
-          /*  href="https://www.instagram.com/yourprofile" */
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a target="_blank" rel="noopener noreferrer">
           <FaInstagram className={styles.icon} />
         </a>
-        <a
-          /* href="https://www.facebook.com/yourprofile" */
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a target="_blank" rel="noopener noreferrer">
           <FaFacebook className={styles.icon} />
         </a>
-        <a
-          /* href="https://wa.me/yourphonenumber" */
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a target="_blank" rel="noopener noreferrer">
           <FaWhatsapp className={styles.icon} />
         </a>
       </div>
--- blackcode/vite.config.js
+++ blackcode/vite.config.js
@@ -4,7 +4,8 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: true, // This makes the server listen on all IP addresses. Useful to access website on phone web browser
-  },
+  // This makes the server listen on all IP addresses. Useful to access website on phone web browser
+  /* server: {
+    host: true,
+  }, */
 });
